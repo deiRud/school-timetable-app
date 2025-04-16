a65788fd3a68b1352f6afedd5f6257cfb92e032a
PARICORP\DRudkova <deina.rudkovaschneider@pari.com>
2025-04-17 00:29:32 +0200

    added icons

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,38 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'], // optional
+      manifest: {
+        name: 'Vivien’s Timetable',
+        short_name: 'Timetable',
+        description: 'A simple school timetable app',
+        theme_color: '#2ca2f0',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
@@ -16,3 +42,5 @@ export default defineConfig({
     },
   },
 })
+
+
